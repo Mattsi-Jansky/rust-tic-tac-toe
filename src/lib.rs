a3134ff8a701614670ba9252c78bfdc044cb9400
Mattsi <mattsi.jansky@codurance.com>
2022-06-17 12:01:51 +0100

    Two tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,47 @@
-struct TicTacToeGame {
+enum Cell {
+    Nort,
+    Cross,
+    None
+}
 
+struct TicTacToeGame {
+    state: [Cell; 9]
 }
 
 impl TicTacToeGame {
     pub fn new() -> TicTacToeGame {
-        TicTacToeGame {}
+            TicTacToeGame { state: [
+                Cell::None,
+                Cell::None,
+                Cell::None,
+                Cell::None,
+                Cell::None,
+                Cell::None,
+                Cell::None,
+                Cell::None,
+                Cell::None,
+            ]
+        }
     }
 
     pub fn display(&self) -> String {
-        String::from(concat!("   \n","   \n","   "))
+        let mut result = String::new();
+
+        for (i, cell) in self.state.iter().enumerate() {
+            result.push_str(match cell {
+                Cell::Nort => "O",
+                Cell::Cross => "X",
+                Cell::None => " "
+            });
+
+            if (i + 1) % 3 == 0 && i < 8 { result.push_str("\n")};
+        }
+
+        result
+    }
+
+    pub fn make_move(&mut self, p0: i32, p1: i32) {
+        self.state[0] = Cell::Nort;
     }
 }
 
@@ -24,6 +57,19 @@ mod tests {
 
         assert_eq!(
             concat!("   \n","   \n","   "),
+            actual
+        )
+    }
+
+    #[test]
+    fn first_move_should_place_x_on_screen() {
+        let mut game = TicTacToeGame::new();
+
+        game.make_move(0,0);
+
+        let actual = game.display();
+        assert_eq!(
+            concat!("O  \n","   \n","   "),
             actual
         )
     }
